d43b18d8e9645b9b1ff6415469746cbf3384e526
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-13 00:04:14 +0300

    styles and more routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginpageComponent } from './loginpage/loginpage.component';
+import { LoginComponent } from './loginpage/loginpage.component';
 import { ProfileComponent } from './profile/profile.component';
 import { SignUppageComponent } from './sign-uppage/sign-uppage.component';
 import { EmailComponent } from './email/email.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'login', component: LoginpageComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'email-login', component: EmailComponent },
   { path: 'signup', component: SignUppageComponent },
   { path: 'profile', component: ProfileComponent },
--- src/app/loginpage/loginpage.component.ts
+++ src/app/loginpage/loginpage.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'app-loginpage',
-  templateUrl: './loginpage.component.html',
-  styleUrls: ['./loginpage.component.css']
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
 })
-export class LoginpageComponent implements OnInit {
+export class LoginComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit(): void {
+  loginGoogle() {
+    this.authService.googleLogin();
   }
-
 }
