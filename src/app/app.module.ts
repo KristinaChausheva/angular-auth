cc9f78a11dff2714d9f3d0ac77dae8493005a696
Kristina Chausheva <k.chausheva@gmail.com>
2022-04-13 02:49:23 +0300

    adding firebase config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,12 @@ import { SignUppageComponent } from './sign-uppage/sign-uppage.component';
 
 import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { environment } from '../environments/environment';
 
 import { AuthService } from './auth.service';
 import { ProfileComponent } from './profile/profile.component';
 import { EmailComponent } from './email/email.component';
-
-const firebaseConfig = {
-  apiKey: 'your_keyhere_jwaeqsFdm1hNMQ',
-  authDomain: 'yourdomain.firebaseapp.com',
-  databaseURL: 'https://angularauth-yourlinkhere.firebaseio.com',
-  projectId: 'angularauth-yourIDHere',
-  storageBucket: 'angularauth-Idhere.appspot.com',
-  messagingSenderId: '11240551_IDhere',
-};
 
 @NgModule({
   declarations: [
@@ -36,7 +27,7 @@ const firebaseConfig = {
   imports: [
     BrowserModule,
     FormsModule,
-    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
   ],
   providers: [AuthService],
